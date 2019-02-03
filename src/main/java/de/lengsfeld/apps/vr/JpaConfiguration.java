1a51c5843bee934c00cb5578da9084b0aad1bb04
RobertTrebor <robert@lengsfeld.de>
2019-02-03 17:30:17 +0100

    remove JPAConfiguration

--- src/main/java/de/lengsfeld/apps/vr/JpaConfiguration.java
+++ src/main/java/de/lengsfeld/apps/vr/JpaConfiguration.java
@@ -1,43 +1,23 @@
 package de.lengsfeld.apps.vr;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.Properties;
 /*
 @Configuration
 @EnableJpaRepositories(basePackages = "de.lengsfeld.apps.vr.repository",
         entityManagerFactoryRef = "entityManagerFactory",
         transactionManagerRef = "transactionManager")*/
 public class JpaConfiguration {
-
+/*
     @Autowired
     private Environment environment;
 
     @Value("${datasource.vr.maxPoolSize:10}")
     private int maxPoolSize;
 
-    /*
+    *//*
      * Populate SpringBoot DataSourceProperties object directly from application.yml
      * based on prefix.Thanks to .yml, Hierachical data is mapped out of the box with matching-name
      * properties of DataSourceProperties object].
-     */
+     *//*
     @Bean
     @Primary
     @ConfigurationProperties(prefix = "datasource.vr")
@@ -45,9 +25,9 @@ public class JpaConfiguration {
         return new DataSourceProperties();
     }
 
-    /*
+    *//*
      * Configure HikariCP pooled DataSource.
-     */
+     *//*
     @Bean
     public DataSource dataSource() {
         DataSourceProperties dataSourceProperties = dataSourceProperties();
@@ -63,7 +43,7 @@ public class JpaConfiguration {
         return dataSource;
     }
 
-    /*
+    *//*
      * Entity Manager Factory setup.
 
     @Bean
@@ -75,19 +55,19 @@ public class JpaConfiguration {
         factoryBean.setJpaProperties(jpaProperties());
         return factoryBean;
     }
-*/
-    /*
+*//*
+    *//*
      * Provider specific adapter.
-     */
+     *//*
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
         HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
         return hibernateJpaVendorAdapter;
     }
 
-    /*
+    *//*
      * Here you can specify any provider specific properties.
-     */
+     *//*
     private Properties jpaProperties() {
         Properties properties = new Properties();
         properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.vr.hibernate.dialect"));
@@ -107,4 +87,5 @@ public class JpaConfiguration {
         txManager.setEntityManagerFactory(emf);
         return txManager;
     }
+    */
 }
