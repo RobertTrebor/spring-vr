54ce02ac0634e943e160f198cbd499e6ecd1abfe
RobertTrebor <robert@lengsfeld.de>
2019-02-03 16:37:08 +0100

    disable startup runner

--- src/main/java/de/lengsfeld/apps/vr/VrApplication.java
+++ src/main/java/de/lengsfeld/apps/vr/VrApplication.java
@@ -1,35 +1,32 @@
 package de.lengsfeld.apps.vr;
 
-import de.lengsfeld.apps.vr.repository.CemeteryRepository;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling
 public class VrApplication {
-
+/*
     protected final Log log = LogFactory.getLog(getClass());
 
     @Autowired
     private CemeteryRepository cemeteryRepository;
 
+
+
     @Scheduled(initialDelay = 1000, fixedRate = 5000)
     public void run() {
         log.info("Number of cemeteries: " + cemeteryRepository.count());
     }
+*/
 
     public static void main(String[] args) {
         SpringApplication.run(VrApplication.class, args);
     }
 
-    @Bean
+/*    @Bean
     public StartupRunner schedulerRunner() {
         return new StartupRunner();
-    }
+    }*/
 }
