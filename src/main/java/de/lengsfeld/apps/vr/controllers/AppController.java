c630f3f5e51dbc40c2233d4b6d67e0e1c495bbe0
rl <r.lengsfeld@gai-netconsult.de>
2019-02-06 13:09:25 +0100

    Move to thymeleaf

--- src/main/java/de/lengsfeld/apps/vr/controllers/AppController.java
+++ src/main/java/de/lengsfeld/apps/vr/controllers/AppController.java
@@ -1,12 +1,21 @@
 package de.lengsfeld.apps.vr.controllers;
 
+import de.lengsfeld.apps.vr.entity.Cemetery;
+import de.lengsfeld.apps.vr.repository.CemeteryRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private CemeteryRepository cemeteryRepository;
 
     @RequestMapping("**/partials/{page}")
     String partialHandler(@PathVariable("page") final String page) {
@@ -19,5 +28,16 @@ public class AppController {
         return "index";
     }
 
+    @GetMapping("/t")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping(value = "cemeteries")
+    public String showCemeteries(Model model){
+        List<Cemetery> cemeteries = cemeteryRepository.findAll();
+        model.addAttribute("cemeteries", cemeteries);
+        return "cemeteries";
+    }
 
 }
