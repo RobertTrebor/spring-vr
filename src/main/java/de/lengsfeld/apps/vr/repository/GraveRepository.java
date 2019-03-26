356e83494c663337aaaa3c4d7bf7d67ca2ad0d44
RobertTrebor <robert@lengsfeld.de>
2019-03-26 08:31:45 +0100

    enable CrossOrigin on repositories

--- src/main/java/de/lengsfeld/apps/vr/repository/GraveRepository.java
+++ src/main/java/de/lengsfeld/apps/vr/repository/GraveRepository.java
@@ -4,9 +4,11 @@ import de.lengsfeld.apps.vr.entity.Cemetery;
 import de.lengsfeld.apps.vr.entity.Grave;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @Repository
 public interface GraveRepository extends JpaRepository<Grave, Long> {
 
